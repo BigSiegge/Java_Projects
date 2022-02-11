c4526c3311d2b2a60c3229d7ca0dbfdf20189c2d
Siegfried Nacion <86394123+BigSiegge@users.noreply.github.com>
2022-02-11 15:27:02 -0800

    Update and rename practice.java to bank_account.java

--- bank_account.java
+++ bank_account.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 import java.io.File;
 
 
-public class practice {
+public class bankAccount {
     public static void main(String[] args) throws IOException {
         Scanner inputFile = new Scanner(new File("accounts.txt"));
         String[] accountNumbers = new String[100];
